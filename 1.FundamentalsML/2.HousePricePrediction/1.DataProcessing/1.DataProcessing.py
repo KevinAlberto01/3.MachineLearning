d74136714ef1173e58fa02218c7bb971260f815d
KevinAlberto01 <kevin.alberto01@outlook.com>
2025-03-03 12:24:23 -0600

    Readme.md

--- 1.FundamentalsML/2.HousePricePrediction/1.DataProcessing/1.DataProcessing.py
+++ 1.FundamentalsML/2.HousePricePrediction/1.DataProcessing/1.DataProcessing.py
@@ -1,11 +1,13 @@
 import pandas as pd 
 import numpy as np 
 
+#1.CARGE THE DATASET
+
 # URL of the dataset 
-url = "https://raw.githubusercontent.com/dsrscientist/DSData/master/housing.csv"
+file_path = "/home/kevin/Desktop/Kevin/3.MachineLearning/1.FundamentalsML/2.HousePricePrediction/1.DataProcessing/AmesHousing.csv"
 
 # Load the dataset
-df = pd.read_csv(url)
+df = pd.read_csv(file_path)
 
 # Show the first rows
 print("First rows of the dataset")
@@ -15,7 +17,16 @@ print(df.head())
 print("\nGeneral Info:")
 print(df.info())
 
-# 
+# Descriptive statistics 
 print("\nDescriptive Statistics:")
 print(df.describe())
+
+#Check for null values 
+print("\nValues missing in each columns")
+print(df.isnull().sum()[df.isnull().sum()>0])
+
+#Initial Shape 
+print(f"\nInitial Shape: {df.shape}")
+
+#2.INITIAL EXPLORING
 
