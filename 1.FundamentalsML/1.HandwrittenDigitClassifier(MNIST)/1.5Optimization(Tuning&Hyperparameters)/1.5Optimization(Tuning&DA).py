af318da3364f4bb7ed5b3ef3a84bd2fc8fc0f2f7
KevinAlberto01 <kevin.alberto01@outlook.com>
2025-03-09 19:29:42 -0600

    Readme

--- 1.FundamentalsML/1.HandwrittenDigitClassifier(MNIST)/1.5Optimization(Tuning&Hyperparameters)/1.5Optimization(Tuning&DA).py
+++ 1.FundamentalsML/1.HandwrittenDigitClassifier(MNIST)/1.5Optimization(Tuning&Hyperparameters)/1.5Optimization(Tuning&DA).py
@@ -1,4 +1,4 @@
-#IMPORT LIBRARIES
+#1.IMPORT LIBRARIES
 import numpy as np 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,21 +11,21 @@ from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 
-#.LOAD DATASET 
+#2.LOAD DATASET 
 print("Loading dataset MNIST 8x8...")
 digits = load_digits()
 x = digits.data
 y = digits.target
 
-#.DIVIDE DATASET INTO TRAINING AND TESTING
+#3.DIVIDE DATASET INTO TRAINING AND TESTING
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
 
-#.NORMALIZE DATA 
+#4.NORMALIZE DATA 
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-#.DEFINE MODELS WITH HYPERPARAMETERS TUNING 
+#5.DEFINE MODELS WITH HYPERPARAMETERS TUNING 
 param_grid = {
     "Logistic Regression": {"C": [0.1, 1, 10], "max_iter": [1000, 5000]},
     "K-Nearest Neighbors (KNN)": {"n_neighbors": [3, 5, 7]},
@@ -37,6 +37,7 @@ results = {}
 conf_matrix = {}
 classification_reports = {}
 
+#6.OPTIMIZE MODELS
 for name, params in param_grid.items():
     print(f"Optimizing {name}...")
     if name == "Logistic Regression":
@@ -61,7 +62,7 @@ for name, params in param_grid.items():
     print(f"Best Parameters for {name}: {grid_search.best_params_}")
     print(f"{name} - Accuracy: {accuracy: .4f}")
 
-#.VISUALIZATION COMPARATION OF ACCURACY 
+#7.VISUALIZATION COMPARATION OF ACCURACY 
 plt.figure(figsize=(8,5))
 plt.bar(results.keys(), results.values(), color = 'skyblue')
 plt.ylabel('Accuracy')
@@ -70,7 +71,7 @@ plt.xticks(rotation = 0)
 plt.ylim(0.95, 1.0)
 plt.show()
 
-#.SHOW CONFUSION MATRIX FOR EACH MODEL
+#7.1 SHOW CONFUSION MATRIX FOR EACH MODEL
 for name, matrix in conf_matrix.items():
     plt.figure(figsize = (6,5))
     sns.heatmap(matrix, annot = True, fmt= 'd', cmap = 'Blues', 
@@ -82,12 +83,13 @@ for name, matrix in conf_matrix.items():
     print(f"Confusion Matrix for {name}:")
     print(matrix)
     print("\nMost Misclassified Digits:")
+    #7.2 Error Analysis
     errors = np.where(matrix != np.diag(matrix))
     for true_label, pred_label in zip(errors[0], errors[1]):
         print (f" - The model confused {true_label} with {pred_label} {matrix[true_label, pred_label]} times.")
     print("-" * 50)
 
-#.CLASSIFICATION REPORT 
+#8.CLASSIFICATION REPORT 
 for name, report in classification_reports.items():
     print(f"\n{name} - Classification Report:")
     print(report)
