67a42cb629f9798024abcba899ece199dc706d92
KevinAlberto01 <kevin.alberto01@outlook.com>
2025-03-10 18:10:28 -0600

    Commit

--- 1.FundamentalsML/1.HandwrittenDigitClassifier(MNIST)/1.8Personalisation/1.8Personalization.py
+++ 1.FundamentalsML/1.HandwrittenDigitClassifier(MNIST)/1.8Personalisation/1.8Personalization.py
@@ -1,3 +1,4 @@
+#1.IMPORT LIBRARIES
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -9,11 +10,12 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import StandardScaler  # Agregado
+from sklearn.preprocessing import StandardScaler  
 
+#2.CONFIGURATION OF STRAMLIT PAGE
 st.set_page_config(layout="wide")
 
-# Ajustar margen superior para pegar el título arriba
+#3.STYLE CSS FOR THE PAGE
 st.markdown(
     """
     <style>
@@ -24,23 +26,24 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-# Título grande y centrado al inicio
+
+#4.PAGE TITLE
 st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; font-size: 42px; font-weight: bold;'>DASHBOARD MNIST</h1>", unsafe_allow_html=True)
 
-# Datos y modelo
+#5.CARGE AND PREPARATION OF THE DATASET
 digits = datasets.load_digits()
 x = digits.data
 y = digits.target
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
 
-# Normalización (StandardScaler que conserva negativos y decimales)
+#Normalization (StandardScaler, conserve negative values) and decimal
 scaler = StandardScaler()
 x_train_normalized = scaler.fit_transform(x_train)
 x_test_normalized = scaler.transform(x_test)
 
-# GridSearchCV y entrenamiento
+#6.TRAINING OF THE MODEL WITH GRIDSEARCH CV
 param_grid = {'C': [1, 10], 'kernel': ['linear', 'rbf']}
 grid_search = GridSearchCV(SVC(), param_grid, cv=3)
 grid_search.fit(x_train_normalized, y_train)  # Importante usar los datos normalizados
@@ -59,7 +62,7 @@ class_counts.columns = ['Class', 'Examples per class']
 df_info_no_index = class_counts
 
 
-# Función para el donut chart
+#7.CREATE THE DONUT CHART (MODEL ACCURACY)
 def make_donut(input_response, input_color='blue'):
     if input_color == 'blue':
         chart_color = ['#E0F0FF', '#2A79B9']
@@ -87,9 +90,11 @@ def make_donut(input_response, input_color='blue'):
     return fig
 
 
-# Streamlit Layout
+# 8.STRUCTURE OF THE COLUMS (STREAMLIT, LAYOUT)
 col1_top, col2_top, col3_top = st.columns([0.7, 3, 1.2])
 
+#9.SHOW INFORMATION IN THE COLUMNS
+#9.1 Column 1
 with col1_top:
     st.markdown("<h3 style='text-align: center;'>Data Info</h3>", unsafe_allow_html=True)
     info_text = f"""
@@ -101,6 +106,7 @@ with col1_top:
     fig_donut = make_donut(accuracy * 100)
     st.pyplot(fig_donut)
 
+#9.2 Column 2
 with col2_top:
     st.markdown("<h3 style='text-align: center;'>True vs Predicted</h3>", unsafe_allow_html=True)
     fig_images, axes = plt.subplots(2, 5, figsize=(12, 5))
@@ -113,6 +119,7 @@ with col2_top:
     plt.subplots_adjust(hspace=0.3)
     st.pyplot(fig_images)
 
+#9.3 Column 3
 with col3_top:
     st.markdown("<h3 style='text-align: center;'>Confusion Matrix</h3>", unsafe_allow_html=True)
     fig_cm, ax_cm = plt.subplots(figsize=(5, 5), dpi=300)
@@ -126,14 +133,17 @@ with col3_top:
     buf.seek(0)
     st.image(Image.open(buf), width=400)
 
+#10.SHOW OTHER DETAILS
 col1_bot, col2_bot, col3_bot, col4_bot = st.columns([0.8, 0.8, 2.2, 1.4])
 
+#10.1 Column 1
 with col1_bot:
     st.markdown("<h3 style='text-align: center;'>Class Distribution</h3>", unsafe_allow_html=True)
     st.markdown("<div style='max-width: 350px; overflow-x: auto;'>", unsafe_allow_html=True)
     st.markdown(df_info_no_index.to_html(index=False), unsafe_allow_html=True)
     st.markdown("</div>", unsafe_allow_html=True)
 
+#10.2 Column 2
 with col2_bot:
     st.markdown("<h3 style='text-align: center;'>Best Parameters</h3>", unsafe_allow_html=True)
     best_params = grid_search.best_params_
@@ -144,7 +154,7 @@ with col2_bot:
     """
     st.markdown(best_params_text)
     st.write(param_grid)
-
+#10.3 Column 3
 with col3_bot:
     st.markdown("<h3 style='text-align: center;'>Normalized Data</h3>", unsafe_allow_html=True)
 
@@ -152,7 +162,7 @@ with col3_bot:
     df_normalized = pd.DataFrame(x_train_normalized[:5], columns=[f"F{i}" for i in range(x_train.shape[1])])
 
     st.write(df_normalized.style.format("{:.4f}"))
-
+#10.4 Column 4
 with col4_bot:
     st.markdown("<h3 style='text-align: center;'>Classification Report</h3>", unsafe_allow_html=True)
     st.markdown("<div style='width: 100%; overflow-x: auto;'>", unsafe_allow_html=True)
