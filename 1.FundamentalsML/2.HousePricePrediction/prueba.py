8725c6936936844d5003e51d9825c8ea3f88812a
Kevin Alberto <132956095+KevinAlberto01@users.noreply.github.com>
2025-06-24 11:52:38 -0600

    Delete 1.FundamentalsML/2.HousePricePrediction directory

--- 1.FundamentalsML/2.HousePricePrediction/prueba.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import numpy as np
-
-modelo_cargado = joblib.load("/home/kevin/Desktop/Kevin/3.MachineLearning/1.FundamentalsML/2.HousePricePrediction/gbm_random.pkl")
-resultado = modelo_cargado.predict([[8]])  # por ejemplo con OverallQual = 8
-print(resultado)
-
-precio_real = 10 ** resultado
-print(precio_real)
