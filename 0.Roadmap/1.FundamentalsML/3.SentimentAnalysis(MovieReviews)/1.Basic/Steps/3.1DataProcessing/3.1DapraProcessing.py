822909f1907995d51c4bbff7ca048b0aaa9ca5d3
KevinAlberto01 <kevin.alberto01@outlook.com>
2025-07-29 09:05:57 -0600

    DataProcessing

--- 0.Roadmap/1.FundamentalsML/3.SentimentAnalysis(MovieReviews)/1.Basic/Steps/3.1DataProcessing/3.1DapraProcessing.py
+++ 0.Roadmap/1.FundamentalsML/3.SentimentAnalysis(MovieReviews)/1.Basic/Steps/3.1DataProcessing/3.1DapraProcessing.py
@@ -1,35 +1,54 @@
+#0.LIBRERIAS 
 import pandas as pd 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
+import re
 
+#################### A.DATA PROCESSING ####################
+#1.Carga de datos 
 df = pd.read_csv("1.Basic/Steps/3.1DataProcessing/IMDB Dataset.csv")
+print()
 
+#2.Inspeccion Inicial
+'''print(df.shape) #tamaño de fila y columna
 print()
-#Inspeccion de tamaño
-'''print(df.shape)
+print(df.info()) #Informacion de la base de datos 
 print()
-print(df.info())
+print(df.describe()) #Describe caracteristicas importantes
 print()
-print(df.describe())
+print(df.head(10)) #Muestra las primeras 10 fila
 print()
-print(df.head(10))
+print(df['sentiment'].unique()) #Checa las categorias 
 print()
-print(df['sentiment'].unique())
+print(df['sentiment'].value_counts()) #Muestra cuantos hay por cada columna
 print()
-print(df['sentiment'].value_counts())
-print()
-print(df.dtypes)
+print(df.dtypes) # Muestra el tipo de datos
 '''
 
-
 #3.Limpieza de datos 
-print(df.drop_duplicates(subset= 'review',inplace=True))
 
+#3.2 Elimina Duplicados
+print(df.drop_duplicates(subset= 'review',inplace=True))
 print()
 
+#3.3 ELIMINACION DEL BR (CASO ESPECIAL NLP)
+df['br_count'] = df['review'].str.count(r'<br\s*/?>', flags=re.IGNORECASE)
+print(df['br_count'].value_counts().sort_index()) #Cuantas filas tienen 0,1,2,3
+print()
+print(df[df['br_count'] > 0]['review'].head())
+print()
+print("Total de <br /> en todo el dataset:", df['br_count'].sum())
+print()
+df['review'] = df['review'].str.replace(r'<br\s*/?>', ' ', regex=True)
+print(df['review'])
+
+#comprobacion de como quedaron las columnas 
+print(df['sentiment'].value_counts()) #Muestra cuantos hay por cada columna
+
+''' 
 #INICIO DEL PASO EXTRA POR SER NLP
 df['sentiment'] = df['sentiment'].map({'positive':1, 'negative':0})
 print(df['sentiment'])
@@ -45,13 +64,15 @@ y = df['sentiment']
 print("Form of X:", x.shape)
 print()
 
-#FIN DEL PASO EXTRA POR SER NLP 
-
-
 #ver palabras mas Importantes
 print(vectorizer.get_feature_names_out()[:20])
 print()
+
+#FIN DEL PASO EXTRA POR SER NLP 
+
 #Division de conjuntos
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 print("Train:", X_train.shape, y_train.shape)
 print("Test:", X_test.shape, y_test.shape)
+
+'''
