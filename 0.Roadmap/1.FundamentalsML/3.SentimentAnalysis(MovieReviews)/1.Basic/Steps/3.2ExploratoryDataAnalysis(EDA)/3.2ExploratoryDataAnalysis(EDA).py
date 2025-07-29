47e796a12344d81724b384492123121d5dbca94a
KevinAlberto01 <kevin.alberto01@outlook.com>
2025-07-29 13:48:50 -0600

    DataProcessing

--- 0.Roadmap/1.FundamentalsML/3.SentimentAnalysis(MovieReviews)/1.Basic/Steps/3.2ExploratoryDataAnalysis(EDA)/3.2ExploratoryDataAnalysis(EDA).py
+++ 0.Roadmap/1.FundamentalsML/3.SentimentAnalysis(MovieReviews)/1.Basic/Steps/3.2ExploratoryDataAnalysis(EDA)/3.2ExploratoryDataAnalysis(EDA).py
@@ -47,15 +47,93 @@ def limpiar_texto(texto):
 
 df['review_clean'] = df['review'].apply(limpiar_texto) #aqui se agrega una nueva columna pero es solamente la correccion
 print("LA BASE DE DATOS AHORA TIENE UN TAMAÑO DE:",df.shape)
+
+#REVISION DEL FORMATO ESTE NOMAS ES PARA CONFIRMAR POR SI VEO UNA ANOMALIA
+#print(df['review_clean'].head(10))
+
 ################# A.FIN DE DATA PROCESSING #################
 
-
-'''
+############### B.EXPLORATORY DATA ANALISYS ################
+# Comparativa del punto 3.4
 # Longitud ANTES de limpiar
 df['length_original'] = df['review'].apply(lambda x: len(x.split()))
 # Longitud DESPUÉS de limpiar
 df['length_clean'] = df['review_clean'].apply(lambda x: len(x.split()))
 
+sns.histplot(df['length_original'], color='blue', label='Original', bins=50, alpha=0.5)
+sns.histplot(df['length_clean'], color='green', label='Cleaned', bins=50, alpha=0.5)
+plt.legend()
+plt.title('Distribución de longitud de reseñas (antes vs después)')
+plt.xlabel('Número de palabras')
+plt.show()
+print("SE GENERO LA GRAFICA COMPARACION DE LONGUITUD ANTES Y DESPUES DE LIMPIAR")
+
+
+#Distribucion de clases sentiment
+sns.countplot(x='sentiment', data= df)
+plt.title('Distribucion de clases (0=Negativo), 1 = Positivo)')
+plt.xticks([0,1], ['Negative', 'Positive'])
+plt.show()
+print("SE GENERO LA GRAFICA DE LA CLASE SENTIMENT")
+
+#Longitud de las reseñas
+df['review_length'] = df['review'].apply(lambda x: len(x.split()))
+sns.histplot(data= df, x='review_length', hue='sentiment', bins=50)
+plt.title('Distribucion de longitud de reseñas')
+plt.xlabel('Numero de palabras')
+plt.ylabel('Frecuencia')
+plt.show()
+
+
+# 3.5 Eliminar stopwords personalizadas
+# Stopwords en inglés + personalizadas
+stop_words = list(text.ENGLISH_STOP_WORDS.union(stopwords.words('english')))
+stop_words += ['movie', 'film', 'one', 'character', 'time', 'story', 'make', 'see',
+    'scene', 'way', 'thing', 'look', 'plot', 'work', 'director', 'watch',
+    'get', 'go', 'going', 'even', 'bit', 'really', 'know', 'think',
+    'much', 'well', 'take', 'still', 'say', 'something', 'lot', 'back',
+    'also', 'end', 'though', 'better', 'people', 'little', 'nothing',
+    'makes', 'right', 'man', 'woman', 'new', 'life', 'im'
+]
+vectorizer = TfidfVectorizer(stop_words=stop_words,max_features=5000)
+#Aplicar vectorizacion al texto 
+
+df = df.dropna(subset=['review_clean'])
+positive_text = ' '.join(df[df['sentiment'] == 'positive']['review_clean'])
+negative_text = ' '.join(df[df['sentiment'] == 'negative']['review_clean'])
+stop_words_set = set(stop_words)
+
+# Limpiar las palabras para WordCloud (eliminar stopwords)
+positive_words = [word for word in positive_text.split() if word not in stop_words_set]
+negative_words = [word for word in negative_text.split() if word not in stop_words_set]
+
+# Volver a unir el texto limpio
+positive_text_cleaned = ' '.join(positive_words)
+negative_text_cleaned = ' '.join(negative_words)
+
+wordcloud_pos = WordCloud(width=800, height=400, background_color='white').generate(positive_text_cleaned)
+wordcloud_neg = WordCloud(width=800, height=400, background_color='black').generate(negative_text_cleaned)
+
+plt.figure(figsize=(15, 6))
+plt.subplot(1, 2, 1)
+plt.imshow(wordcloud_pos, interpolation='bilinear')
+plt.title('Palabras más comunes (Positivas)')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(wordcloud_neg, interpolation='bilinear')
+plt.title('Palabras más comunes (Negativas)')
+plt.axis('off')
+plt.show()
+
+
+
+'''
+
+x = vectorizer.fit_transform(df['review'])
+#Etiquetas
+y = df['sentiment']
+print()
 
 
 #5.TRANSFORMACION DE VARIABLES CATEGORICAS (PARA NLP)
@@ -67,15 +145,6 @@ print(df.dtypes)
 print() 
 #Crear el vectorizador
 
-
-
-vectorizer = TfidfVectorizer(stop_words='english',max_features=5000)
-#Aplicar vectorizacion al texto 
-x = vectorizer.fit_transform(df['review'])
-#Etiquetas
-y = df['sentiment']
-print("Form of X:", x.shape)
-print()
 
 #FIN DEL PASO EXTRA POR SER NLP 
 
